90497a7285b1746560b22c2763057ea17827593f
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-08-09 17:56:51 -0500

    Changed the default random engine to be a lot simpler, and simply provide access to a shared instance and some creation functions to create instances of the standard engine type.

---
+++ Assets/Plugins/Experilous/Randomization/RandomEngine.cs
@@ -0,0 +1,61 @@
+﻿/******************************************************************************\
+* Copyright Andy Gainey                                                        *
+\******************************************************************************/
+
+namespace Experilous.Randomization
+{
+	public static class RandomEngine
+	{
+		private static IRandomEngine _shared = null;
+
+		public static IRandomEngine shared
+		{
+			get
+			{
+				if (_shared == null)
+				{
+					_shared = CreateStandard();
+				}
+				return _shared;
+			}
+			set
+			{
+				if (value == null)
+				{
+					throw new System.NullReferenceException();
+				}
+				_shared = value;
+			}
+		}
+
+		public static XorShift128Plus CreateStandard()
+		{
+			return XorShift128Plus.Create();
+		}
+
+		public static XorShift128Plus CreateStandard(int seed)
+		{
+			return XorShift128Plus.Create(seed);
+		}
+
+		public static XorShift128Plus CreateStandard(params int[] seed)
+		{
+			return XorShift128Plus.Create(seed);
+		}
+
+		public static XorShift128Plus CreateStandard(string seed)
+		{
+			return XorShift128Plus.Create(seed);
+		}
+
+		public static XorShift128Plus CreateStandard(RandomStateGenerator stateGenerator)
+		{
+			return XorShift128Plus.Create(stateGenerator);
+		}
+
+		public static XorShift128Plus CreateStandard(IRandomEngine seeder)
+		{
+			return XorShift128Plus.Create(seeder);
+		}
+	}
+}
