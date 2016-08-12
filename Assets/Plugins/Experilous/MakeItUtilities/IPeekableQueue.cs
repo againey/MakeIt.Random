1b4c58eefbfcdd46972f5d6468b41e51c414cb89
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-08-11 23:08:43 -0500

    First phase in transitioning to the "Make It" branding, which involves new folders and moving existing files, unchanged into the new folders.

---
+++ Assets/Plugins/Experilous/MakeItUtilities/IPeekableQueue.cs
@@ -0,0 +1,19 @@
+﻿/******************************************************************************\
+* Copyright Andy Gainey                                                        *
+\******************************************************************************/
+
+namespace Experilous.Containers
+{
+	/// <summary>
+	/// A container supporting abstract push and pop actions for inserting
+	/// and removing elements in an implementation-defined order, and allowing
+	/// the front item to be accessed without popping it.
+	/// </summary>
+	/// <typeparam name="T">The type of the elements in the container.</typeparam>
+	public interface IPeekableQueue<T> : IQueue<T>
+	{
+		T front { get; }
+		T Peek();
+		void RemoveFront();
+	}
+}
---
+++ Assets/Plugins/Experilous/MakeItUtilities/IQueue.cs
@@ -0,0 +1,20 @@
+﻿/******************************************************************************\
+* Copyright Andy Gainey                                                        *
+\******************************************************************************/
+
+namespace Experilous.Containers
+{
+	/// <summary>
+	/// A container supporting abstract push and pop actions for inserting
+	/// and removing elements in an implementation-defined order.
+	/// </summary>
+	/// <typeparam name="T">The type of the elements in the container.</typeparam>
+	public interface IQueue<T>
+	{
+		bool isEmpty { get; }
+		int Count { get; }
+		void Push(T item);
+		T Pop();
+		void Clear();
+	}
+}
