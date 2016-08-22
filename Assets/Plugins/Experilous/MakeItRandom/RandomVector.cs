dab0f83610101c897d380110976147f4c70de61b
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-08-22 16:49:53 -0500

    Consolidated some shared portions of 32-bit and 64-bit code in RandomVector.         This will make it easier to inline these functions where desired, such as for calculating circular or spherical shells.

--- Assets/Plugins/Experilous/MakeItRandom/RandomVector.cs
+++ Assets/Plugins/Experilous/MakeItRandom/RandomVector.cs
@@ -48,7 +48,6 @@ namespace Experilous.MakeItRandom
 #else
 #if OPTIMIZE_FOR_32
 			// Inline of RandomVector.PointWithinCircle()
-
 			Start:
 			uint lower, upper;
 			random.Next64(out lower, out upper);
@@ -69,27 +68,8 @@ namespace Experilous.MakeItRandom
 			// If 32-bit version is greater than a certain threshold, then the full 64-bit version might reach or go
 			// over the length of 1 even if the 32-bit version doesn't due to bit truncation.
 			if (uvSqrScaled >= 0x3FFF0000 && uvSqr >= 0x4000000000000000L) goto Start; // x^2 + y^2 > r^2, so generated point is not inside the circle.
-
-			// Formula is from http://mathworld.wolfram.com/CirclePointPicking.html
-			// uvSqr starts as a Q62 fixed point number, but we want our numerators to have
-			// twice as many bits as our denominator, so we'll shift uvSqr to become a Q31.
-			uvSqr = uvSqr >> 31;
-			int x = (int)((uSqr - vSqr) / uvSqr);
-			int y = (int)(((u * v) / uvSqr) << 1);
-			// The end result is that x and y are Q31.
-
-			// Inline of Detail.FloatingPoint.FixedToFloat()
-			Detail.FloatingPoint.BitwiseFloat conv;
-			conv.bits = 0U;
-			conv.number = x;
-			conv.bits -= 0x0F800000U; // exponent -= 31
-			vec.x = conv.number;
-			conv.number = y;
-			conv.bits -= 0x0F800000U; // exponent -= 31
-			vec.y = conv.number;
 #else
 			// Inline of RandomVector.PointWithinCircle()
-
 			Start:
 			ulong bits = random.Next64();
 			uint upper = (uint)(bits >> 32);
@@ -100,7 +80,8 @@ namespace Experilous.MakeItRandom
 			long vSqr = v * v;
 			long uvSqr = uSqr + vSqr;
 			if (uvSqr >= 0x4000000000000000L) goto Start; // x^2 + y^2 > r^2, so generated point is not inside the circle.
-			
+#endif
+
 			// Formula is from http://mathworld.wolfram.com/CirclePointPicking.html
 			// uvSqr starts as a Q62 fixed point number, but we want our numerators to have
 			// twice as many bits as our denominator, so we'll shift uvSqr to become a Q31.
@@ -118,7 +99,6 @@ namespace Experilous.MakeItRandom
 			conv.number = y;
 			conv.bits -= 0x0F800000U; // exponent -= 31
 			vec.y = conv.number;
-#endif
 #endif
 		}
 
@@ -154,87 +134,19 @@ namespace Experilous.MakeItRandom
 			// fixed point form up until the end to maintain speed and bit-level reliability.  Final
 			// conversion to float is designed to get maximum possible precision for numbers near zero.
 
-#if OPTIMIZE_FOR_32
+
 			// Find a point inside a circle, modified inline of RandomVector.PointWithinCircle()
 			Start:
+
+#if OPTIMIZE_FOR_32
 			uint lower, upper;
 			random.Next64(out lower, out upper);
-			// Using 26 bits of each of the 2D components, that gives us 2^52 possible positions in 2D space.
-			// This is a half-closed square, so positions with u = +1 or v = +1 never occur, but that's okay
-			// because we reject points that are exactly on the edge of the circle anyway.  But technically,
-			// any point exactly on the circumference should map to (0, 0, -1) using Marsaglia's formula, and
-			// no other (u, v) pair inside the circle will.  So we increase the number of possible states by
-			// one, and in that rare 1 out of 2^52 + 1 times, we return exactly (0, 0, -1).
-			// 1/2^6 * 1/2^6 * 2^12/(2^52+1) = 1/(2^52+1)
-			if (upper >= 0xFC000000U && lower >= 0xFC000000U && random.RangeCO(0x0010000000000001UL) < 0x0000000000001000UL)
-			{
-				vec.x = 0f;
-				vec.y = 0f;
-				vec.z = -1f;
-				return;
-			}
-			int u = (int)(upper & 0x03FFFFFFU) - 0x02000000; // x*2^25
-			int v = (int)(lower & 0x03FFFFFFU) - 0x02000000; // y*2^25
-			int uScaled = u >> 8;
-			int vScaled = v >> 8;
-			int uvSqrScaled = uScaled * uScaled + vScaled * vScaled;
-			// First do a check against the 32-bit radius before doing a full 64-bit calculation
-			if (uvSqrScaled > 0x10000000) goto Start; // x^2 + y^2 >= r^2, so generated point is not inside the circle.
-
-			ulong uSqr = (ulong)((long)u * u); // x^2 * 2^50
-			ulong vSqr = (ulong)((long)v * v); // y^2 * 2^50
-			ulong uvSqr = uSqr + vSqr; // (x^2 + y^2) * 2^50
-			if (uvSqrScaled > 0x0FFF8000 && uvSqr >= 0x0000100000000000UL) goto Start; // x^2 + y^2 >= r^2, so generated point is not inside the circle.
-
-			ulong uvSqrInv = (0x0004000000000000UL - uvSqr) << 12; // (1 - (x^2 + y^2)) * 2^62
-
-			// Calculate the square root of uvSqrInv.  This starts with an approximation found at
-			//   http://stackoverflow.com/a/1100591
-			// It is followed by two uses of the divide-and-average method to improve the initial approximation.
-
-			// Begin with an inline of Detail.DeBruijnLookup.GetBitMaskForRangeMax()
-			ulong mask = uvSqrInv | (uvSqrInv >> 1);
-			mask |= mask >> 2;
-			mask |= mask >> 4;
-			mask |= mask >> 8;
-			mask |= mask >> 16;
-			mask |= mask >> 32;
-			int bitCount = Detail.DeBruijnLookup.bitCountTable64[mask * Detail.DeBruijnLookup.multiplier64 >> Detail.DeBruijnLookup.shift64];
-
-			// Lookup sqrt(a) (the portion of the square root determined by the magnitude of the number)
-			ulong sqrtA = Detail.FloatingPoint.fastSqrtUpper[bitCount]; // a * 2^31
-			// Lookup sqrt(b) (the square root of a number between 1 and 2, using 6 bits worth of data)
-			ulong sqrtB = Detail.FloatingPoint.fastSqrtLower[(bitCount >= 7 ? (uvSqrInv >> (bitCount - 7)) : (uvSqrInv << (7 - bitCount))) & 0x3FU]; // b * 2^31
-			// Square root is a*b
-			ulong uvInv = (sqrtA * sqrtB) >> 31; // a * b * 2^31 = sqrt(1 - (x^2 + y^2)) * 2^31
-
-			// Improve the square root approximation using the divide-and-average method twice
-			uvInv = (uvSqrInv / uvInv + uvInv) >> 1; // sqrt(1 - (x^2 + y^2)) * 2^31, better approximation
-			uvInv = (uvSqrInv / uvInv + uvInv); // 2 * sqrt(1 - (x^2 + y^2)) * 2^31, even better approximation, multiplied by 2
-
-			// Determine the final components using Marsaglia's formulas:  t = 2 * sqrt(1 - (u^2 + v^2)); x = u*t; y = v*t; z = 1 - 2 * (u^2 + v^2))
-			long x = u * (long)uvInv; // x * 2^25 * 2 * sqrt(1 - (x^2 + y^2)) * 2^31 = 2 * x * sqrt(1 - (x^2 + y^2)) * 2^56
-			long y = v * (long)uvInv; // y * 2^25 * 2 * sqrt(1 - (x^2 + y^2)) * 2^31 = 2 * y * sqrt(1 - (x^2 + y^2)) * 2^56
-			long z = 0x0004000000000000L - ((long)uvSqr << 1); // 2 * (1/2 - (x^2 + y^2)) * 2^50
-
-			// Inline of Detail.FloatingPoint.FixedToFloat()
-			Detail.FloatingPoint.BitwiseFloat conv;
-			conv.bits = 0U;
-			conv.number = x;
-			conv.bits -= 0x1C000000U; // exponent -= 56
-			vec.x = conv.number;
-			conv.number = y;
-			conv.bits -= 0x1C000000U; // exponent -= 56
-			vec.y = conv.number;
-			conv.number = z;
-			conv.bits -= 0x19000000U; // exponent -= 50
-			vec.z = conv.number;
 #else
-			// Find a point inside a circle, modified inline of RandomVector.PointWithinCircle()
-			Start:
 			ulong bits = random.Next64();
 			uint upper = (uint)(bits >> 32);
 			uint lower = (uint)bits;
+#endif
+
 			// Using 26 bits of each of the 2D components, that gives us 2^52 possible positions in 2D space.
 			// This is a half-closed square, so positions with u = +1 or v = +1 never occur, but that's okay
 			// because we reject points that are exactly on the edge of the circle anyway.  But technically,
@@ -251,12 +163,28 @@ namespace Experilous.MakeItRandom
 				vec.z = -1f;
 				return;
 			}
+
+#if OPTIMIZE_FOR_32
+			int u = (int)(upper & 0x03FFFFFFU) - 0x02000000; // x*2^25
+			int v = (int)(lower & 0x03FFFFFFU) - 0x02000000; // y*2^25
+			int uScaled = u >> 8;
+			int vScaled = v >> 8;
+			int uvSqrScaled = uScaled * uScaled + vScaled * vScaled;
+			// First do a check against the 32-bit radius before doing a full 64-bit calculation
+			if (uvSqrScaled > 0x10000000) goto Start; // x^2 + y^2 >= r^2, so generated point is not inside the circle.
+
+			ulong uSqr = (ulong)((long)u * u); // x^2 * 2^50
+			ulong vSqr = (ulong)((long)v * v); // y^2 * 2^50
+			ulong uvSqr = uSqr + vSqr; // (x^2 + y^2) * 2^50
+			if (uvSqrScaled > 0x0FFF8000 && uvSqr >= 0x0000100000000000UL) goto Start; // x^2 + y^2 >= r^2, so generated point is not inside the circle.
+#else
 			long u = (upper & 0x03FFFFFFU) - 0x02000000L; // x*2^25
 			long v = (lower & 0x03FFFFFFU) - 0x02000000L; // y*2^25
 			ulong uSqr = (ulong)(u * u); // x^2 * 2^50
 			ulong vSqr = (ulong)(v * v); // y^2 * 2^50
 			ulong uvSqr = uSqr + vSqr; // (x^2 + y^2) * 2^50
 			if (uvSqr >= 0x0004000000000000UL) goto Start; // x^2 + y^2 >= r^2, so generated point is not inside the circle.
+#endif
 
 			ulong uvSqrInv = (0x0004000000000000UL - uvSqr) << 12; // (1 - (x^2 + y^2)) * 2^62
 
@@ -301,7 +229,6 @@ namespace Experilous.MakeItRandom
 			conv.number = z;
 			conv.bits -= 0x19000000U; // exponent -= 50
 			vec.z = conv.number;
-#endif
 #endif
 		}
 
@@ -400,10 +327,17 @@ namespace Experilous.MakeItRandom
 			var distance = Mathf.Sqrt(random.ClosedFloatUnit());
 			v = random.UnitVector2() * distance;
 #else
-#if OPTIMIZE_FOR_32
 			Start:
+
+#if OPTIMIZE_FOR_32
 			uint lower, upper;
 			random.Next64(out lower, out upper);
+#else
+			ulong bits = random.Next64();
+			uint upper = (uint)(bits >> 32);
+			uint lower = (uint)bits;
+#endif
+
 			// Using 23 bits of each of the 2D components, that gives us 2^46 possible positions in 2D space.
 			// This is a half-closed square, so positions with u = +1 or v = +1 never occur.  To compensate,
 			// we increase the number of possible states by two, and in those rare 2 out of 2^46 + 2 times,
@@ -414,6 +348,8 @@ namespace Experilous.MakeItRandom
 				vec = random.Chance() ? new Vector2(1f, 0f) : new Vector2(0f, 1f);
 				return;
 			}
+
+#if OPTIMIZE_FOR_32
 			int u = (int)(upper & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
 			int v = (int)(lower & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
 			int uScaled = u >> 8;
@@ -424,33 +360,11 @@ namespace Experilous.MakeItRandom
 			{
 				goto Start; // x^2 + y^2 > r^2, so generated point is outside the circle.
 			}
-
-			Detail.FloatingPoint.BitwiseFloat value;
-			value.number = 0f;
-			value.bits = Detail.FloatingPoint.floatTwo | Detail.FloatingPoint.floatMantissaMask & upper;
-			vec.x = value.number - 3f;
-			value.bits = Detail.FloatingPoint.floatTwo | Detail.FloatingPoint.floatMantissaMask & lower;
-			vec.y = value.number - 3f;
 #else
-			Start:
-			ulong bits = random.Next64();
-			uint upper = (uint)(bits >> 32);
-			uint lower = (uint)bits;
-			// Using 23 bits of each of the 2D components, that gives us 2^46 possible positions in 2D space.
-			// This is a half-closed square, so positions with u = +1 or v = +1 never occur.  To compensate,
-			// we increase the number of possible states by two, and in those rare 2 out of 2^46 + 2 times,
-			// we return exactly (+1, 0) or (0, +1).
-			// 1/2^9 * 1/2^9 * (2 * 2^18)/(2^46+2) = 1/2^9 * 1/2^9 * 2^18/(2^45+1) = 2/(2^46+2)
-			// We divide the first 17-bit check into a 9-bit check and 8-bit check so that the 32-bit version
-			// of this function is more efficient.
-			if (upper >= 0xFF800000U && lower >= 0xFF800000U && random.RangeCO(0x0000200000000001UL) < 0x0000000000040000UL)
-			{
-				vec = random.Chance() ? new Vector2(1f, 0f) : new Vector2(0f, 1f);
-				return;
-			}
-			long ix = (int)(upper & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
-			long iy = (int)(lower & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
-			if (ix * ix + iy * iy > 0x0000100000000000L) goto Start; // x^2 + y^2 > r^2, so generated point is outside the circle.
+			long u = (int)(upper & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
+			long v = (int)(lower & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
+			if (u * u + v * v > 0x0000100000000000L) goto Start; // x^2 + y^2 > r^2, so generated point is outside the circle.
+#endif
 
 			Detail.FloatingPoint.BitwiseFloat value;
 			value.number = 0f;
@@ -458,7 +372,6 @@ namespace Experilous.MakeItRandom
 			vec.x = value.number - 3f;
 			value.bits = Detail.FloatingPoint.floatTwo | Detail.FloatingPoint.floatMantissaMask & lower;
 			vec.y = value.number - 3f;
-#endif
 #endif
 		}
 
@@ -527,11 +440,20 @@ namespace Experilous.MakeItRandom
 			var distance = Mathf.Pow(random.ClosedFloatUnit(), 1f / 3f);
 			return random.UnitVector3() * distance;
 #else
-#if OPTIMIZE_FOR_32
 			Start:
+
+#if OPTIMIZE_FOR_32
 			uint ux, uy, uz, uw;
 			random.Next64(out ux, out uy);
 			random.Next64(out uz, out uw);
+#else
+			ulong uxuy = random.Next64();
+			ulong uzuw = random.Next64();
+			uint ux = (uint)uxuy;
+			uint uy = (uint)(uxuy >> 32);
+			uint uz = (uint)uzuw;
+#endif
+
 			// Using 23 bits of each of the 3D components, that gives us 2^69 possible positions in 3D space.
 			// This is a half-closed cube, so positions (+1, 0, 0), (0, +1, 0), and (0, 0, +1) never occur.
 			// To compensate, we increase the number of possible states by three, and in those rare 3 out of
@@ -542,7 +464,13 @@ namespace Experilous.MakeItRandom
 			// me calculate that quickly:  http://www.numberempire.com/numberfactorizer.php), we can adjust the
 			// random chance rolls as follows:
 			// 1/2^41 * 1/2^9 * 1/2^9 * 2^40/282149961813509 * (3 * 2^19)/(5 * 418427) = 3/(2^69+3)
-			if (uw == 0xFFFFFFFFU && uz >= 0xFF800000UL && ux >= 0xFF800000U && uy >= 0xFF800000U && random.Probability(0x0000010000000000UL, 282149961813509UL) && random.Probability(0x00180000U, 2092135U))
+			if (
+#if OPTIMIZE_FOR_32
+				uw == 0xFFFFFFFFU && uz >= 0xFF800000UL && ux >= 0xFF800000U && uy >= 0xFF800000U
+#else
+				uzuw >= 0xFFFFFFFFFF800000UL && ux >= 0xFF800000U && uy >= 0xFF800000U
+#endif
+				&& random.Probability(0x0000010000000000UL, 282149961813509UL) && random.Probability(0x00180000U, 2092135U))
 			{
 				switch (random.RangeCO(3U))
 				{
@@ -552,6 +480,8 @@ namespace Experilous.MakeItRandom
 					default: throw new System.InvalidOperationException();
 				}
 			}
+
+#if OPTIMIZE_FOR_32
 			int ix = (int)(ux & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
 			int iy = (int)(uy & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
 			int iz = (int)(uz & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
@@ -563,46 +493,12 @@ namespace Experilous.MakeItRandom
 			{
 				goto Start; // x^2 + y^2 + z^2 > r^2, so generated point is outside the sphere.
 			}
-
-			Detail.FloatingPoint.BitwiseFloat value;
-			value.number = 0f;
-			value.bits = Detail.FloatingPoint.floatTwo | Detail.FloatingPoint.floatMantissaMask & ux;
-			vec.x = value.number - 3f;
-			value.bits = Detail.FloatingPoint.floatTwo | Detail.FloatingPoint.floatMantissaMask & uy;
-			vec.y = value.number - 3f;
-			value.bits = Detail.FloatingPoint.floatTwo | Detail.FloatingPoint.floatMantissaMask & uz;
-			vec.z = value.number - 3f;
 #else
-			Start:
-			ulong uxuy = random.Next64();
-			ulong uzuw = random.Next64();
-			uint ux = (uint)uxuy;
-			uint uy = (uint)(uxuy >> 32);
-			uint uz = (uint)uzuw;
-			// Using 23 bits of each of the 3D components, that gives us 2^69 possible positions in 3D space.
-			// This is a half-closed cube, so positions (+1, 0, 0), (0, +1, 0), and (0, 0, +1) never occur.
-			// To compensate, we increase the number of possible states by three, and in those rare 3 out of
-			// 2^69 + 3 times, we return one of those special three positions.
-			// 1/2^41 * 1/2^9 * 1/2^9 * (3 * 2^59)/(2^69+3) = 3/(2^69+3)
-			// That's impossible to directly calculate with 64-bit integers, but thanks to the fact that 2^69+3
-			// can be factored as 5 * 418427 * 282149961813509, (and thanks to the following website for letting
-			// me calculate that quickly:  http://www.numberempire.com/numberfactorizer.php), we can adjust the
-			// random chance rolls as follows:
-			// 1/2^41 * 1/2^9 * 1/2^9 * 2^40/282149961813509 * (3 * 2^19)/(5 * 418427) = 3/(2^69+3)
-			if (uzuw >= 0xFFFFFFFFFF800000UL && ux >= 0xFF800000U && uy >= 0xFF800000U && random.Probability(0x0000010000000000UL, 282149961813509UL) && random.Probability(0x00180000U, 2092135U))
-			{
-				switch (random.RangeCO(3U))
-				{
-					case 0U: vec = new Vector3(1f, 0f, 0f); return;
-					case 1U: vec = new Vector3(1f, 0f, 0f); return;
-					case 2U: vec = new Vector3(1f, 0f, 0f); return;
-					default: throw new System.InvalidOperationException();
-				}
-			}
 			long ix = (int)(ux & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
 			long iy = (int)(uy & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
 			long iz = (int)(uz & Detail.FloatingPoint.floatMantissaMask) - 0x00400000;
 			if (ix * ix + iy * iy + iz * iz > 0x0000100000000000L) goto Start; // x^2 + y^2 + z^2 > r^2, so generated point is outside the sphere.
+#endif
 
 			Detail.FloatingPoint.BitwiseFloat value;
 			value.number = 0f;
@@ -612,7 +508,6 @@ namespace Experilous.MakeItRandom
 			vec.y = value.number - 3f;
 			value.bits = Detail.FloatingPoint.floatTwo | Detail.FloatingPoint.floatMantissaMask & uz;
 			vec.z = value.number - 3f;
-#endif
 #endif
 		}
 
