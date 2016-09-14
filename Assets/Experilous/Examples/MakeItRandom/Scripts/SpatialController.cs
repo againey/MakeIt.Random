86a5d90e679ca58d58f48c3849f3e760832245e5
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-09-13 23:56:32 -0500

    Improved the random spatial demo scene. - Changed to a particle system with billboards. - Switched from a skybox to a totally black background. - Added rotation to fully visualize spatial distribution. - Added a particle sprite to easily show center and orientation. - Added button for random rotation distribution.

--- Assets/Experilous/Examples/MakeItRandom/Scripts/SpatialController.cs
+++ Assets/Experilous/Examples/MakeItRandom/Scripts/SpatialController.cs
@@ -10,8 +10,7 @@ namespace Experilous.Examples.MakeItRandom
 	public class SpatialController : MonoBehaviour
 	{
 		public Transform pointObjectPrefab;
-
-		public int pointObjectCount = 128;
+		public new ParticleSystem particleSystem;
 
 		public Color baseColor = new Color(0.6f, 0.3f, 0.1f);
 		public float hueRange = 0.1f;
@@ -21,101 +20,194 @@ namespace Experilous.Examples.MakeItRandom
 		public float narrowShellThickness = 0.2f;
 		public float wideShellThickness = 0.5f;
 
+		public float rotationSpeed = 1f;
+
 		private XorShift128Plus _random;
 
-		private Transform[] _pointObjects;
+		private ParticleSystem.Particle[] _particles;
+
+		private bool _flat;
+		private AnimationCurve _flatRotationRate;
 
 		protected void Awake()
 		{
 			_random = XorShift128Plus.Create();
-			_pointObjects = new Transform[pointObjectCount];
 
-			for (int i = 0; i < _pointObjects.Length; ++i)
+			_particles = new ParticleSystem.Particle[particleSystem.maxParticles];
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				_pointObjects[i] = Instantiate(pointObjectPrefab);
-				_pointObjects[i].SetParent(transform, false);
+				_particles[i].angularVelocity = 0f;
+				_particles[i].lifetime = float.PositiveInfinity;
+				_particles[i].size = 0.1f;
+				_particles[i].startLifetime = 0f;
+				_particles[i].velocity = Vector3.zero;
 			}
+
+			PositionAtUnitVectors3D();
+
+			_flatRotationRate = new AnimationCurve(
+				new Keyframe(0f, 0f, 0f, 0f),
+				new Keyframe(0.3f, 0f, 0f, 0f),
+				new Keyframe(0.7f, 1f, 0f, 0f),
+				new Keyframe(1f, 1f, 0f, 0f));
+		}
+
+		protected void Update()
+		{
+			float rotation = Time.time * rotationSpeed;
+			if (_flat)
+			{
+				float t = Mathf.Repeat(rotation, 0.5f) * 2f;
+				rotation = _flatRotationRate.Evaluate(t) * 0.5f + (Mathf.Repeat(rotation, 1f) < 0.5f ? 0f : 0.5f);
+			}
+			else
+			{
+				rotation = Mathf.Repeat(rotation, 1f);
+			}
+			particleSystem.transform.localRotation = Quaternion.AngleAxis(rotation * 360f, Vector3.up);
+		}
+
+		private void Set2D()
+		{
+			particleSystem.SetParticles(_particles, _particles.Length);
+			particleSystem.Play();
+			particleSystem.Pause();
+
+			_flat = true;
+		}
+
+		private void Set3D()
+		{
+			particleSystem.SetParticles(_particles, _particles.Length);
+			particleSystem.Play();
+			particleSystem.Pause();
+
+			_flat = false;
 		}
 
 		public void PositionAtUnitVectors2D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.UnitVector2();
+				_particles[i].position = _random.UnitVector2();
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 
 		public void PositionWithinCircle2D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinCircle();
+				_particles[i].position = _random.PointWithinCircle();
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 
 		public void PositionWithinThinShell2D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinCircularShell(1f - narrowShellThickness, 1f);
+				_particles[i].position = _random.PointWithinCircularShell(1f - narrowShellThickness, 1f);
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 
 		public void PositionWithinThickShell2D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinCircularShell(1f - wideShellThickness, 1f);
+				_particles[i].position = _random.PointWithinCircularShell(1f - wideShellThickness, 1f);
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 
 		public void PositionAtUnitVectors3D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.UnitVector3();
+				_particles[i].position = _random.UnitVector3();
+				_particles[i].rotation = 0f;
 			}
+
+			Set3D();
 		}
 
 		public void PositionWithinSphere3D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinSphere();
+				_particles[i].position = _random.PointWithinSphere();
+				_particles[i].rotation = 0f;
 			}
+
+			Set3D();
 		}
 
 		public void PositionWithinThinShell3D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinSphericalShell(1f - narrowShellThickness, 1f);
+				_particles[i].position = _random.PointWithinSphericalShell(1f - narrowShellThickness, 1f);
+				_particles[i].rotation = 0f;
 			}
+
+			Set3D();
 		}
 
 		public void PositionWithinThickShell3D()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinSphericalShell(1f - wideShellThickness, 1f);
+				_particles[i].position = _random.PointWithinSphericalShell(1f - wideShellThickness, 1f);
+				_particles[i].rotation = 0f;
 			}
+
+			Set3D();
+		}
+
+		public void PositionWithRandomRotation()
+		{
+			for (int i = 0; i < _particles.Length; ++i)
+			{
+				float angle;
+				Vector3 axis;
+				_random.Rotation().ToAngleAxis(out angle, out axis);
+				_particles[i].position = axis;
+				_particles[i].rotation = angle;
+			}
+
+			Set3D();
 		}
 
 		public void PositionWithinSquare()
 		{
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinSquare(2f) - Vector2.one;
+				_particles[i].position = _random.PointWithinSquare(2f) - Vector2.one;
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 
 		public void PositionWithinRectangle()
 		{
 			Vector2 size = new Vector2(_random.RangeCC(1f, 2f), _random.RangeCC(1f, 2f));
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinRectangle(size) - size * 0.5f;
+				_particles[i].position = _random.PointWithinRectangle(size) - size * 0.5f;
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 
 		public void PositionWithinParallelogram()
@@ -130,10 +222,13 @@ namespace Experilous.Examples.MakeItRandom
 			}
 			axis0 *= _random.RangeCC(1f, 2f);
 			axis1 *= _random.RangeCC(1f, 2f);
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinParallelogram(axis0, axis1) - (axis0 + axis1) * 0.5f;
+				_particles[i].position = _random.PointWithinParallelogram(axis0, axis1) - (axis0 + axis1) * 0.5f;
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 
 		public void PositionWithinTriangle()
@@ -149,10 +244,13 @@ namespace Experilous.Examples.MakeItRandom
 			axis0 *= _random.RangeCC(1f, 2f);
 			axis1 *= _random.RangeCC(1f, 2f);
 			Vector2 offset = (axis0 + axis1) / 3f;
-			foreach (var pointObject in _pointObjects)
+			for (int i = 0; i < _particles.Length; ++i)
 			{
-				pointObject.position = _random.PointWithinTriangle(axis0, axis1) - offset;
+				_particles[i].position = _random.PointWithinTriangle(axis0, axis1) - offset;
+				_particles[i].rotation = 0f;
 			}
+
+			Set2D();
 		}
 	}
 }
